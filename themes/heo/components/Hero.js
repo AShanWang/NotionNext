65545a95194e3f4f3b5c837d3d1dd53e914ed8d6
Ashan <129077870+AShanWang@users.noreply.github.com>
2023-09-11 14:42:36 +0800

    修改heo英雄区右边的图片

--- themes/heo/components/Hero.js
+++ themes/heo/components/Hero.js
@@ -379,7 +379,7 @@ function TodayCard({ cRef }) {
           } cursor-pointer today-card-cover absolute w-full h-full top-0`}
           style={{
             background:
-              "url('https://bu.dusays.com/2023/03/12/640dcd3a1b146.png') no-repeat center /cover"
+              "url('https://23img.com/i/2023/09/11/ntl6sj.png') no-repeat center /cover"
           }}
         ></div>
       </div>
