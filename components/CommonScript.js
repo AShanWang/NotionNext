97f4301ecb7f53195a590281ae13f8da89618da0
Ashan <129077870+AShanWang@users.noreply.github.com>
2023-09-13 10:19:58 +0800

    复原

--- components/CommonScript.js
+++ components/CommonScript.js
@@ -48,11 +48,6 @@ const CommonScript = () => {
 
     {BLOG.COMMENT_TIDIO_ID && <script async src={`//code.tidio.co/${BLOG.COMMENT_TIDIO_ID}.js`} />}
 
-    {/* 新添加的代码块 */}
-    <script defer data-domain="pengchao.eu.org" src="https://tj.ashanwpc.eu.org/js/script.js"></script>
-
-    {/* ... 其他代码 ... */}
-
 
     {/* gitter聊天室 */}
     {BLOG.COMMENT_GITTER_ROOM && (<>
