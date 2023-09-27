15d2054f15efafe82e8e262b69d4fe336fcde6e0
Ashan <1753002287@qq.com>
2023-09-27 23:10:37 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -40,7 +40,8 @@ module.exports = withBundleAnalyzer({
       'images.unsplash.com',
       'source.unsplash.com',
       'p1.qhimg.com',
-      'webmention.io'
+      'webmention.io',
+      'i0.hdslb.com'
     ]
   },
   // 默认将feed重定向至 /public/rss/feed.xml
