016ddcdd379567a5d562c57d9b227012b720ef71
Ashan <ashancbt@gmail.com>
2024-11-27 15:52:28 +0800

    Update PoweredBy.js【改➤ 、链接、用心】

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -9,13 +9,10 @@ export default function PoweredBy(props) {
     <div
       className={`gap-x-1 flex flex-wrap text-sm font-serif ${props.className || ''}`}>
       <span>➤ </span>
-      // <span>Powered by</span>
       <a
         href='https://jyjc.cloudns.org'
-        // href='https://github.com/tangly1024/NotionNext'
         className='underline justify-start'>
         用心做好每一期教程！
-        // NotionNext {siteConfig('VERSION')}
       </a>
       .
     </div>
